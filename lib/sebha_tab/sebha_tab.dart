9aa85da6a47355945e95cf4a081b1f8fa696b99c
[Abdullah] <[Abdullah@gmail.com]>
2024-12-25 15:49:05 +0200

    sebha

--- lib/sebha_tab/sebha_tab.dart
+++ lib/sebha_tab/sebha_tab.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  Widget sebhaBody = Image.asset('assets/PNG Images/SebhaBody 1.png');
+  int zekrCount = 0;
+  String zekr = 'سبحان الله';
+  double rotationAngle = 0; // Rotation angle in radians
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -9,13 +20,80 @@ class SebhaTab extends StatelessWidget {
           Text(
             'سَبِّحِ اسْمَ رَبِّكَ الأعلى ',
             style: TextStyle(
-              fontSize: 36,
+              fontSize: 30,
               color: Colors.white,
               fontWeight: FontWeight.w700,
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                tasbee7();
+              },
+              child: Container(
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 58),
+                      child: Image.asset('assets/PNG Images/sebha_head.png'),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Transform.rotate(
+                          angle: rotationAngle, // Apply rotation here
+                          child: sebhaBody,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          zekr,
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          '$zekrCount',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void tasbee7() {
+    setState(() {
+      zekrCount++;
+      rotationAngle += 90 * math.pi / 100; // Increment the rotation angle
+      switch (zekrCount) {
+        case 33:
+          zekr = 'الحمد لله';
+          break; // Add break to avoid falling through
+        case 66:
+          zekr = 'الله أكبر';
+          break;
+        case 99:
+          zekrCount = 0;
+          zekr = 'سبحان الله';
+          break;
+      }
+    });
   }
 }
